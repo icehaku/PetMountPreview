d5ea321ee90812953a5c0426751b91e4d762c88d
Elano Saraiva Garcez <elanogarcez@gmail.com>
2025-12-27 00:38:14 -0300

    POC base, mostrando log com nome e ID do item

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,4 +1,4 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
@@ -17,6 +17,8 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static IPlayerState PlayerState { get; private set; } = null!;
     [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+    [PluginService] internal static IGameGui GameGui { get; private set; } = null!;
+    [PluginService] internal static IFramework Framework { get; private set; } = null!;
 
     private const string CommandName = "/pmycommand";
 
@@ -25,12 +27,12 @@ public sealed class Plugin : IDalamudPlugin
     public readonly WindowSystem WindowSystem = new("SamplePlugin");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
+    private ulong lastHoveredItem;
 
     public Plugin()
     {
         Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
-        // You might normally want to embed resources and load them from the manifest stream
         var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
 
         ConfigWindow = new ConfigWindow(this);
@@ -44,29 +46,22 @@ public sealed class Plugin : IDalamudPlugin
             HelpMessage = "A useful message to display in /xlhelp"
         });
 
-        // Tell the UI system that we want our windows to be drawn through the window system
         PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
-
-        // This adds a button to the plugin installer entry of this plugin which allows
-        // toggling the display status of the configuration ui
         PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUi;
-
-        // Adds another button doing the same but for the main ui of the plugin
         PluginInterface.UiBuilder.OpenMainUi += ToggleMainUi;
 
-        // Add a simple message to the log with level set to information
-        // Use /xllog to open the log window in-game
-        // Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
         Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+        Framework.Update += OnFrameworkUpdate;
     }
 
     public void Dispose()
     {
-        // Unregister all actions to not leak anything during disposal of plugin
+        Framework.Update -= OnFrameworkUpdate;
+
         PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
         PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
         PluginInterface.UiBuilder.OpenMainUi -= ToggleMainUi;
-        
+
         WindowSystem.RemoveAllWindows();
 
         ConfigWindow.Dispose();
@@ -77,10 +72,69 @@ public sealed class Plugin : IDalamudPlugin
 
     private void OnCommand(string command, string args)
     {
-        // In response to the slash command, toggle the display status of our main ui
         MainWindow.Toggle();
     }
-    
+
+    private void OnFrameworkUpdate(IFramework framework)
+    {
+        var hovered = GameGui.HoveredItem;
+
+        if (hovered == 0)
+        {
+            lastHoveredItem = 0;
+            return;
+        }
+
+        if (hovered == lastHoveredItem)
+            return;
+
+        lastHoveredItem = hovered;
+
+        var itemSheet = DataManager.GetExcelSheet<Lumina.Excel.Sheets.Item>();
+        if (itemSheet == null)
+            return;
+
+        if (!itemSheet.TryGetRow((uint)hovered, out var item))
+            return;
+
+        var itemName = item.Name.ToString();
+        Log.Information($"Hovered Item: {itemName} ({hovered})");
+
+        var itemActionRef = item.ItemAction;
+        if (!itemActionRef.IsValid)
+            return;
+
+        var itemAction = itemActionRef.Value;
+
+        // 🐾 MINION
+        if (itemAction.RowId == 853)
+        {
+            var minionId = itemAction.Data[0];
+            var minion = DataManager
+                .GetExcelSheet<Lumina.Excel.Sheets.Companion>()?
+                .GetRow(minionId);
+
+            Log.Information(
+                $"MINION: {minion?.Singular ?? "Unknown"} (ID: {minionId})"
+            );
+            return;
+        }
+
+        // 🐎 MOUNT
+        if (itemAction.RowId == 1322)
+        {
+            var mountId = itemAction.Data[0];
+            var mount = DataManager
+                .GetExcelSheet<Lumina.Excel.Sheets.Mount>()?
+                .GetRow(mountId);
+
+            Log.Information(
+                $"MOUNT: {mount?.Singular ?? "Unknown"} (ID: {mountId})"
+            );
+            return;
+        }
+    }
+
     public void ToggleConfigUi() => ConfigWindow.Toggle();
     public void ToggleMainUi() => MainWindow.Toggle();
 }
